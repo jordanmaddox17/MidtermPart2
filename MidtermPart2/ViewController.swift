a6057e40d0a8903d8fa1a06493cfb25cec5ec675
Jordan <jordanjean17@gmail.com>
2020-04-08 17:29:16 -0700

    Initial Commit

---
+++ MidtermPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidtermPart2
+//
+//  Created by Jordan Maddox on 4/8/20.
+//  Copyright © 2020 Jordan Maddox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
